f755fe4d9cdd44f70d934f1d49df99d4881c10e6
MatthewHeun <matthew.heun@me.com>
2014-02-25 10:53:47 -0500

    Now using "with" in the residuals function to make code much more readable.

--- DAETesting.R
+++ DAETesting.R
@@ -1,4 +1,5 @@
 require(deSolve)
+require(FME)
 require(lattice)
 
 
@@ -17,10 +18,8 @@ require(lattice)
 #   y1_0 = 0
 
 # Let's define the parameters first
-params <- c(a=5, b=2)
+parms <- c(a=5, b=2)
 
-# Now set the initial state
-y_init <- c(y1=0, y2=2)
 # Specify the times at which we would like solutions reported
 solveTimes <- seq(0, 2, 0.1) 
 
@@ -30,28 +29,31 @@ solveTimes <- seq(0, 2, 0.1)
 # Internally, it calculates the initial value for the derivatives at time 0.
 # I have buried the residuals function inside simpleSystemSolve, so that
 # the code is nicely compartmentalized.
-simpleSystemSolve <- function(times=solveTimes, 
-                              y_init=c(y1=0, y2=as.vector(y1+parms["b"])), 
-                              parms=c(a=5, b=2)){
+simpleModel <- function(times, 
+                        y_init=c(y1=0, y2=as.vector(y1+parms["b"])), 
+                        parms=c(a=5, b=2)){
 
   # Function that calculates residuals.
   # Bury inside simpleSystemSolve, because we don't need it anywhere else.
-  residuals <- function(t, y, dy, parms){
-    a <- parms[["a"]]
-    b <- parms[["b"]]
-    dy1dt <- dy[["y1"]]
-    y1 <- y[["y1"]]
-    y2 <- y[["y2"]]
-    res1 <- dy1dt - a
-    res2 <- y2 - y1 - b
-    out <- list(c(res1, res2), a=a, b=b)
-    return(out)
+  residuals <- function(t, y, dydt, parms){
+    # The block of code to calculate the residuals goes here.
+    with(as.list(c(y, dydt, parms)), {
+      # The "with" statement allows us to simplify the equations.
+      # Because we're using "with," we can refer to the variables by their names
+      # in the y, dydt, and parms numeric lists.
+      # We don't need to de-reference them with statemens such as
+      # a <- parms[["a"]]
+      # Be careful for name collisions, though.
+      res1 <- dy1dt - a
+      res2 <- y2 - y1 - b
+      out <- list(c(res1, res2), a=a, b=b)
+      return(out)
+    })
   }
   # Find a consistent set of derivatives at the initial time.
   # For this problem, it is easy. By inspection, we can what the derivatives are that the initial time.
   # But, for more complicated systems, we may need to execute a solve step.
-#   dydt_init <- c(y1=parms["a"])
-  dydt_init <- c(y1=5)
+  dydt_init <- c(dy1dt=as.vector(parms["a"]))
   # Use the residual function and the initial state and derivatives
   # to integrate the DAE system forward in time with the daspk function.
   result <- daspk(y=y_init, 
@@ -64,10 +66,10 @@ simpleSystemSolve <- function(times=solveTimes,
 }
 
 # Run the model
-result <- simpleSystemSolve()
+result <- simpleModel(times=solveTimes)
 
-# For a graph, type the following into the console:
-# xyplot(y1+y2~time, data=df)
+# Show a graph of the results
+print(xyplot(y1+y2~time, data=result))
 
 # If we know some "historical" data for y1 and y2, can we estimate values for a and b?
 # Add some random noise to the historical data.
@@ -77,6 +79,12 @@ historical$y1_act <- seq(0, 10, 0.5) + rnorm(sd=0.1, n=length(historical$time))
 historical$y2_act <- seq(2, 12, 0.5) + rnorm(sd=0.1, n=length(historical$time))
 
 # Need to define a "cost" function.
-# costFunc <- function(parms){
-#   pred <- 
+# We'll use the modCost function in package FME to help with this.
+# The model "cost" is a function of the parameters
+# simpleModelCostFunc <- function(parms){
+#   pred <- simpleModel(times=solveTimes, parms=parms)
+#   cost <- modCost(model=pred, obs=historical, err="sd")
 # }
+# 
+# # Try the cost function
+# cost <- simpleModelCostFunc(parms)
