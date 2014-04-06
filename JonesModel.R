48e7c29164775aaf930465b39ee3f7445ea7f7ac
MatthewHeun <matthew.heun@me.com>
2014-04-06 15:01:42 -0400

    Now solving for tau in ssResid.

--- JonesModel.R
+++ JonesModel.R
@@ -94,21 +94,18 @@ ssResid <- function(p, constraints){
     N <- approx(x=data$Year, y=data$N, xout=year)$y # Interpolate to find N at year
     L_Y <- approx(x=data$Year, y=data$L_Y, xout=year)$y #Interpolate to find L_Y at year
     L <- approx(x=data$Year, y=data$L, xout=year)$y #Interpolate to find L at year
-    tau <- approx(x=data$Year, y=data$tau, xout=year)$y #Interpolate to find tau at year
 
     R1 <- as.vector(Y/Y_0 - y) # y = Y/Y_0   as.vector() strips off the name
     R2 <- as.vector(N/N_0 - n) # n = N/N_0
     R3 <- as.vector(L_Y/L_Y_0 - l_Y) # l_Y = L_Y/L_Y_0
     R4 <- as.vector(L/L_0 - l) #l = L/L_0
-#     R5 <- as.vector(L/N - tau) #tau = L/N
-#     return(c(R1=R1, R2=R2, R3=R3, R4=R4, R5=R5))
-    return(c(R1=R1, R2=R2, R3=R3, R4=R4))
+    R5 <- as.vector(L/N - tau) #tau = L/N
+    return(c(R1=R1, R2=R2, R3=R3, R4=R4, R5=R5))
   })
 }
 
-# p_init <- c(y=0, n=0, l_Y=0, l=0, tau=0) # Initial guess for the parameters that will be solved
-p_init <- c(y=0, n=0, l_Y=0, l=0) # Initial guess for the parameters that will be solved
-ssParms <- c(dadt=0, dndt=0, year=1985.5) # Constraint parameters for the model
+p_init <- c(y=0, n=0, l_Y=0, l=0, tau=0) # Initial guess for the parameters that will be solved
+ssParms <- c(dadt=0, dndt=0, year=1980) # Constraint parameters for the model
 ssModel <- BBsolve(p=p_init, fn=ssResid, constraints=ssParms)
 print(ssModel$par)
 
